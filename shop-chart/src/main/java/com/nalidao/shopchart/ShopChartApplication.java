1ef9185e1f90fd572d608bca0a32a2d592700f2b
Paulo Henrique Correa dos Santos <paulo.santos@engdb.com.br>
2019-11-11 11:50:22 -0300

    API de Carrinho - primeiro commit

---
+++ shop-chart/src/main/java/com/nalidao/shopchart/ShopChartApplication.java
@@ -0,0 +1,13 @@
+package com.nalidao.shopchart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopChartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopChartApplication.class, args);
+	}
+
+}
---
+++ shop-chart/src/test/java/com/nalidao/shopchart/ShopChartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nalidao.shopchart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopChartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
