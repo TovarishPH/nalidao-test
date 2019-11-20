fca429b4e75adb0dba7e49ad7f8258cad1f96b7a
danilotimoteo <olinadtcosta@gmail.com>
2019-11-20 03:29:20 -0300

    Testa o metodo findAll do ProdutoService

--- products/src/test/java/com/nalidao/products/service/ProdutoServiceTest.java
+++ products/src/test/java/com/nalidao/products/service/ProdutoServiceTest.java
@@ -1,6 +1,12 @@
 package com.nalidao.products.service;
 
-import static org.mockito.Mockito.when;
+import com.nalidao.products.domain.Produto;
+import com.nalidao.products.gateway.ProdutoGateway;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -8,92 +14,64 @@ import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.junit.jupiter.MockitoExtension;
-
-import com.nalidao.products.domain.Produto;
-import com.nalidao.products.errorhandling.exception.ProdutoNotFoundException;
-import com.nalidao.products.gateway.ProdutoGateway;
-import com.nalidao.products.repository.ProdutoRepository;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class ProdutoServiceTest {
-	
+
 	@InjectMocks
 	private ProdutoService service;
-	
-	@InjectMocks
-	private ProdutoGateway gateway;
-	
+
 	@Mock
-	private ProdutoRepository repository;
-	
-	private List<Produto> produtos;
-	
-	@BeforeEach
-	public void init() {
-		this.service = new ProdutoService(gateway);
-		this.gateway = new ProdutoGateway(repository);
-		
-		this.produtos = this.getListDeProdutosMock();
-	}
+	private ProdutoGateway gateway;
 
 	@Test
 	public void testFindAllProdutos() {
-		when(this.repository.findAll()).thenReturn(this.produtos);
-		when(this.gateway.findAll()).thenReturn(this.produtos);
-		
-		assertThat(this.service.findAll()).isNotNull().isNotEmpty();
-		assertEquals(2, this.produtos.size());
+		//Dado uma lista de produtos existentes (given)
+		List<Produto> listDeProdutosMock = getListDeProdutosMock();
+		when(this.gateway.findAll()).thenReturn(listDeProdutosMock);
+
+		//Quando estes produtos forem consultados (when)
+		List<Produto> produtosEncontrados = this.service.findAll();
+
+		//Entao os produtos deverão ser retornados (then)
+		assertThat(produtosEncontrados).isNotNull().isNotEmpty();
+		assertEquals(produtosEncontrados.size(), listDeProdutosMock.size());
 	}
-	
+
 	@Test
 	public void testFindById() {
 		Produto produto = this.getProduto();
-		
-		when(this.repository.findById(1l)).thenReturn(Optional.of(produto));
+
 		when(this.gateway.findById(1l)).thenReturn(Optional.of(produto));
-		
+
 		assertThat(this.service.findById(1l)).contains(produto);
 	}
-	
-//	@Test
-//	public void testFindByIdException() {
-//		Mockito.doThrow(IllegalArgumentException.class).when(this.repository.findById(1l));
-//		Mockito.doThrow(IllegalArgumentException.class).when(this.gateway.findById(1l));
-//		Mockito.doThrow(ProdutoNotFoundException.class).when(this.service.findById(1l));
-//	}
-	
+
 	@Test
 	public void testCreate() {
 		//Comparando tamanho da lista depois de executar o método save
 //		assertEquals(3, this.produtos.size());
 	}
-	
+
 	private List<Produto> getListDeProdutosMock() {
 		List<Produto> produtos = new ArrayList<Produto>();
-		
+
 		Produto p1 = new Produto("Produto 1", 1.5, 5);
 		p1.setId(1l);
 		produtos.add(p1);
-		
+
 		Produto p2 = new Produto("Produto 2", 3.5, 2);
 		p2.setId(2l);
 		produtos.add(p2);
-		
+
 		return produtos;
 	}
-	
+
 	private Produto getProduto() {
 		Produto p = new Produto("Produto", 100.5, 10);
 		p.setId(1l);
-		
+
 		return p;
 	}
 }
